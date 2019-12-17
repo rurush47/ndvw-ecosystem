61bfd32143fdd4c1a7a369394df218f0fce5e0fd
anaishoareau <49558242+anaishoareau@users.noreply.github.com>
2019-12-17 12:23:57 +0100

    Create PreyController.cs

---
+++ Assets/Scripts/Animal/PreyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class PreyController : AnimalController 
+{
+	new void Update () 
+	{
+		base.Update();
+	}
+
+	new void FixedUpdate() {
+		base.FixedUpdate();
+	}
+}
