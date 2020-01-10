ba90fec74906b7bbd4844a6f209fe85d20639131
Aleksander Rurarz <alex@lichthund.com>
2020-01-10 07:10:39 +0100

    Add billboard canvas script

---
+++ Assets/Scripts/Utils/Billboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    void Update()
+    {
+        
+//        transform.LookAt(Camera.main.transform);       
+        transform.LookAt(SceneView.GetAllSceneCameras()[0].transform);       
+    }
+}
